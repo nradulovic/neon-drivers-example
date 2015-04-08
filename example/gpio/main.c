487d00b4aa15cc81784e9a22dfc1defb62135e66
Nenad Radulovic <nenad.b.radulovic@gmail.com>
2015-04-09 01:51:02 +0200

    Compilation fixes

--- example/gpio/main.c
+++ example/gpio/main.c
@@ -4,6 +4,7 @@
 
 /*=========================================================  INCLUDE FILES  ==*/
 
+#include "neon_eds.h"
 
 /*=========================================================  LOCAL MACRO's  ==*/
 /*======================================================  LOCAL DATA TYPES  ==*/
@@ -13,10 +14,30 @@
 /*============================================  LOCAL FUNCTION DEFINITIONS  ==*/
 /*===========================================  GLOBAL FUNCTION DEFINITIONS  ==*/
 
+
 int main(void)
 {
 
     return (0);
+}
+
+
+
+PORT_C_NORETURN
+void hook_at_assert(
+    const struct ncomponent_info * component_info,
+    const char *                fn,
+    uint32_t                    line,
+    const char *                expr,
+    const char *                msg)
+{
+    (void)component_info;
+    (void)fn;
+    (void)line;
+    (void)expr;
+    (void)msg;
+
+    for (;;);
 }
 
 /*================================*//** @cond *//*==  CONFIGURATION ERRORS  ==*/
